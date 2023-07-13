9c79cd9c5d69f036869fa043446154b4412afa4e
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-07-13 15:38:19 +0200

    Make a final structure for apps

--- beneighb/beneighb/urls.py
+++ beneighb/beneighb/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from beneighb.apps.upload.views import image_upload
+from apps.upload.views import image_upload
 
 urlpatterns = [
     path("admin/", admin.site.urls),
