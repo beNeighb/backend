0f2dff928217c56539643461d9036934c48d6904
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-07-19 17:24:32 +0200

    Add docker-compose-images.prod.yml

--- start_prod.sh
+++ start_prod.sh
@@ -1,5 +1,5 @@
-docker-compose -f docker-compose.prod.yml down -v
-docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose-images.prod.yml down -v
+docker-compose -f docker-compose-images.prod.yml up -d
 sleep 3
 docker-compose exec web python manage.py migrate --noinput
 docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
