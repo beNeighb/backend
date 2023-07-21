7fa08c55fd6784786bb97a372653d664d9be4af5
Mero <mhryshyn@gmail.com>
2023-07-21 09:49:04 +0000

    Make script more reliable

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,14 +5,14 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(beneighb.com www.beneighb.com)
+domain=api.beneighb.com
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mhryshyn@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for $domain. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
@@ -27,58 +27,64 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="./data/certbot/conf/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+echo "### Creating dummy certificate for $domain ..."
+path="./data/certbot/conf/live/$domain"
+echo "PAAAAATH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "$path/privkey.pem"
+mkdir -p "$data_path/conf/live/$domain"
 # docker-compose run --rm --entrypoint "\
 #   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
 #     -keyout '$path/privkey.pem' \
 #     -out '$path/fullchain.pem' \
 #     -subj '/CN=localhost'" certbot
+  # openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+  #   -keyout '$path/privkey.pem' \
+  #   -out '$path/fullchain.pem' \
+  #   -subj '/CN=localhost'
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
+    -keyout './data/certbot/conf/live/api.beneighb.com/privkey.pem' \
+    -out './data/certbot/conf/live/api.beneighb.com/fullchain.pem' \
     -subj '/CN=localhost'
 echo
 
 
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-echo
+#echo "### Starting nginx ..."
+#docker-compose up --force-recreate -d nginx
+#echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+#echo "### Deleting dummy certificate for $domain ..."
+#docker-compose run --rm --entrypoint "\
+#  rm -Rf /etc/letsencrypt/live/$domain && \
+#  rm -Rf /etc/letsencrypt/archive/$domain && \
+#  rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+#echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+#echo "### Requesting Let's Encrypt certificate for $domain ..."
+##Join $domains to -d args
+#domain_args=""
+#for domain in "${domains[@]}"; do
+#  domain_args="$domain_args -d $domain"
+#done
 
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
+## Select appropriate email arg
+#case "$email" in
+#  "") email_arg="--register-unsafely-without-email" ;;
+#  *) email_arg="--email $email" ;;
+#esac
 
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+## Enable staging mode if needed
+#if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
+#docker-compose run --rm --entrypoint "\
+#  certbot certonly --webroot -w /var/www/certbot \
+#    $staging_arg \
+#    $email_arg \
+#    $domain_args \
+#    --rsa-key-size $rsa_key_size \
+#    --agree-tos \
+#    --force-renewal" certbot
+#echo
 
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+#echo "### Reloading nginx ..."
+#docker-compose exec nginx nginx -s reload
