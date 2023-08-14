4961a7c2dc937cb5d78ce808762c5e1bb63ba18e
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-08-14 17:11:39 +0200

    Black urls

--- beneighb/apps/auth0/urls.py
+++ beneighb/apps/auth0/urls.py
@@ -8,15 +8,13 @@ from apps.auth0.views import DummyView, BeneighbPasswordResetConfirmView
 
 
 urlpatterns = [
-    path("dummy/", DummyView.as_view(), name="dummy-view"),
-
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-
-    path("password-reset/", PasswordResetView.as_view()),
+    path('dummy/', DummyView.as_view(), name='dummy-view'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password-reset/', PasswordResetView.as_view()),
     path(
-        "password-reset-confirm/<uidb64>/<token>/",
+        'password-reset-confirm/<uidb64>/<token>/',
         BeneighbPasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
+        name='password_reset_confirm',
     ),
 ]
