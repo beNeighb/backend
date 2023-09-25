57d9d47c192ce93588e308665f1cf24562709f85
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-09-25 18:01:35 +0200

    chore: Remove leftover from dj_rest_auth implementation

--- beneighb/apps/auth0/serializers.py
+++ beneighb/apps/auth0/serializers.py
@@ -20,22 +20,6 @@ class LinkPasswordResetSerializer(PasswordResetSerializer):
 
         return value
 
-    # TODO: do we still need it
-    def get_email_options(self):
-        extra_context = {
-            'email_site_name': 'Beneighb',
-            'site_protocol': settings.SITE_PROTOCOL,
-        }
-
-        return {
-            'domain_override': settings.SITE_LINK_URL,
-            'email_template_name': 'reset_password_email.html',
-            # We'll need it for sure
-            # 'html_email_template_name': 'reset_password_email.html',
-            'extra_email_context': extra_context,
-            'subject_template_name': 'password_reset_subject.txt',
-        }
-
 
 class VerifiedEmailSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
