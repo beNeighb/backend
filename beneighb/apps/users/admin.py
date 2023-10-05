4327dd43462925c3c610cc3edc8c46255f9bb234
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-10-05 15:50:15 +0200

    feat: Add Profile to Django admin

--- beneighb/apps/users/admin.py
+++ beneighb/apps/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile
 
 # Register your models here.
 admin.site.register(
@@ -11,4 +11,15 @@ admin.site.register(
         'email_verified',
     ],
     read_only=['email_verified'],
+)
+
+admin.site.register(
+    Profile,
+    list_display=[
+        'name',
+        'age_above_18',
+        'agreed_with_conditions',
+        'gender',
+        'speaking_languages',
+    ],
 )
