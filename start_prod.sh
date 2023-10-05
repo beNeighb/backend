f2a0e9a22d549a6a3a06384bfb3cf1cd53a7a636
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-10-05 15:47:03 +0200

    chore: Add docker system prune to run on each deploy

--- start_prod.sh
+++ start_prod.sh
@@ -1,6 +1,10 @@
+docker system prune -af
+
 docker-compose -f docker-compose-images.prod.yml pull
 docker-compose -f docker-compose-images.prod.yml down -v
 docker-compose -f docker-compose-images.prod.yml up -d
+
 sleep 3
+
 docker-compose exec web python manage.py migrate --noinput
 docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
