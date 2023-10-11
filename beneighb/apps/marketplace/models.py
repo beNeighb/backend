c3adba245259f100c60391ecabb9941b4725c147
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-10-11 20:28:17 +0200

    fix: Change Category representation in admin

--- beneighb/apps/marketplace/models.py
+++ beneighb/apps/marketplace/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     )
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = "Categories"
 
