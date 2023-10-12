2eb2138fa6785eb6bac79d26431be5e53a1c671a
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-10-12 18:11:19 +0200

    fix: Add all needed languages

--- beneighb/apps/users/models.py
+++ beneighb/apps/users/models.py
@@ -13,21 +13,49 @@ class Genders(models.TextChoices):
 
 
 class LanguageChoices(models.TextChoices):
-    ESPERANTO = 'eo', 'Esperanto'
-    UKRAINIAN = 'uk', 'Ukrainian'
-    ENGLISH = 'en', 'English'
-    ARABIC = 'ar', 'Arabic'
-    FRENCH = 'fr', 'French'
-    SPANISH = 'es', 'Spanish'
-    GERMAN = 'de', 'German'
-    ITALIAN = 'it', 'Italian'
-    CHINESE = 'zh', 'Chinese'
-    JAPANESE = 'ja', 'Japanese'
-    KOREAN = 'ko', 'Korean'
-    PORTUGUESE = 'pt', 'Portuguese'
-    TURKISH = 'tr', 'Turkish'
-    POLISH = 'pl', 'Polish'
-    RUSSIAN = 'ru', 'Russian'
+    AFRIKAANS = ('af', 'Afrikaans')
+    ARABIC = ('ar', 'Arabic')
+    ARMENIAN = ('hy', 'Armenian')
+    BENGALI = ('bn', 'Bengali')
+    BULGARIAN = ('bg', 'Bulgarian')
+    CHINESE = ('zh', 'Chinese')
+    CROATIAN = ('hr', 'Croatian')
+    CZECH = ('cs', 'Czech')
+    DANISH = ('da', 'Danish')
+    DUTCH = ('nl', 'Dutch')
+    ENGLISH = ('en', 'English')
+    ESTONIAN = ('et', 'Estonian')
+    ESPERANTO = ('eo', 'Esperanto')
+    FARSI = ('fa', 'Farsi')
+    FINNISH = ('fi', 'Finnish')
+    FRENCH = ('fr', 'French')
+    GEORGIAN = ('ka', 'Georgian')
+    GERMAN = ('de', 'German')
+    GREEK = ('el', 'Greek')
+    HINDI = ('hi', 'Hindi')
+    HUNGARIAN = ('hu', 'Hungarian')
+    INDONESIAN = ('id', 'Indonesian')
+    ITALIAN = ('it', 'Italian')
+    JAPANESE = ('ja', 'Japanese')
+    KAZAKH = ('kk', 'Kazakh')
+    KOREAN = ('ko', 'Korean')
+    KURDISH = ('ku', 'Kurdish')
+    MACEDONIAN = ('mk', 'Macedonian')
+    NORWEGIAN = ('no', 'Norwegian')
+    POLISH = ('pl', 'Polish')
+    PORTUGUESE = ('pt', 'Portuguese')
+    ROMANIAN = ('ro', 'Romanian')
+    RUSSIAN = ('ru', 'Russian')
+    SLOVAK = ('sk', 'Slovak')
+    SLOVENIAN = ('sl', 'Slovenian')
+    SPANISH = ('es', 'Spanish')
+    SWEDISH = ('sv', 'Swedish')
+    THAI = ('th', 'Thai')
+    TURKISH = ('tr', 'Turkish')
+    UKRAINIAN = ('uk', 'Ukrainian')
+    URDU = ('ur', 'Urdu')
+    UZBEK = ('uz', 'Uzbek')
+    VIETNAMESE = ('vi', 'Vietnamese')
 
 
 class Profile(models.Model):
