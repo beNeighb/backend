d32fa498b4d33a55bb295d9166fb8cda8d506c44
Myroslav Hryshyn <myroslav.hryshyn@fareharbor.com>
2023-11-09 10:19:00 +0100

    fix: Make /marketplace/tasks/ visible again

--- beneighb/apps/marketplace/urls.py
+++ beneighb/apps/marketplace/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 from apps.marketplace.views import TaskView
 
 
+# TODO: Find a better way to put this urls in one
 urlpatterns = [
     path(
         'tasks/<int:pk>/',
+        TaskView.as_view(),
+        name='task',
+    ),
+    path(
+        'tasks/',
         TaskView.as_view(),
         name='tasks',
     ),
