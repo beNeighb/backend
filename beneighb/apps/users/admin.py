ae9fbc026f2705645a99d1796314693f06ac98d4
Myroslav Hryshyn <mhryshyn@gmail.com>
2023-11-26 08:36:43 +0100

    fix(admin): Add form for setting password for add/change User in admin

--- beneighb/apps/users/admin.py
+++ beneighb/apps/users/admin.py
@@ -1,17 +1,37 @@
 from django.contrib import admin
-from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(
-    User,
-    list_display=[
-        'email',
-        'first_name',
-        'last_name',
-        'email_verified',
-    ],
-    read_only=['email_verified'],
-)
+from .models import Profile, User
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'email_verified']
+    readonly_fields = ['email_verified']
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                ),
+            },
+        ),
+    )
+
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+
+        # Exclude the password field from the list of fields
+        fields = [field for field in fields if field != 'password']
+
+        return fields
+
+
+admin.site.register(User, CustomUserAdmin)
+
 
 admin.site.register(
     Profile,
