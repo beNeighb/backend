d4fd2c2516279156863ed21ba4d8b9c22af04e4b
Myroslav Hryshyn <mhryshyn@gmail.com>
2023-12-04 17:52:35 +0100

    feat(admin): Add Offer with validation to admin

--- beneighb/apps/marketplace/admin.py
+++ beneighb/apps/marketplace/admin.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
+from copy import deepcopy
+from django.contrib import admin, messages
 
-from .models import ServiceCategory, Service, Task
+from apps.users.models import Profile
+from .models import Offer, ServiceCategory, Service, Task
+from .serializers import OfferSerializer
 
 
 admin.site.register(
@@ -35,3 +38,52 @@ admin.site.register(
         'price_offer',
     ],
 )
+
+
+class OfferAdminSerializer(OfferSerializer):
+    """
+    In OfferSerializer we get helper from context and set status to pending.
+    Here we rely on data from admin form.
+    """
+
+    def __init__(self, *args, **kwargs):
+        data = kwargs['data']
+        self.helper = Profile.objects.get(id=data['helper'])
+
+        # Do not want to call OfferSerializer.__init__ here
+        super(OfferSerializer, self).__init__(*args, **kwargs)
+
+    def is_valid(self, *args, **kwargs):
+        # Do not want to call OfferSerializer.is_valid here
+        return super(OfferSerializer, self).is_valid(*args, **kwargs)
+
+
+class OfferAdmin(admin.ModelAdmin):
+    model = Offer
+
+    def save_model(self, request, obj, form, change):
+        data_for_serializer = deepcopy(form.cleaned_data)
+        data_for_serializer['helper'] = data_for_serializer['helper'].id
+        data_for_serializer['task'] = data_for_serializer['task'].id
+
+        serializer = OfferAdminSerializer(data=data_for_serializer)
+
+        if serializer.is_valid():
+            obj.save()
+        else:
+            error_messages = ', '.join(
+                [
+                    item
+                    for sublist in serializer.errors.values()
+                    for item in sublist
+                ]
+            )
+
+            self.message_user(
+                request,
+                f'Validation error: {error_messages}',
+                level=messages.ERROR,
+            )
+
+
+admin.site.register(Offer, OfferAdmin)
--- beneighb/apps/marketplace/serializers/offer.py
+++ beneighb/apps/marketplace/serializers/offer.py
@@ -7,19 +7,21 @@ class OfferSerializer(serializers.ModelSerializer):
         model = Offer
         fields = '__all__'
 
-    def _get_helper_profile(self):
-        return self.context['request'].user.profile
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = self.context['request'].user.profile
 
     def is_valid(self, *args, **kwargs):
         if hasattr(self.initial_data, '_mutable'):
             self.initial_data._mutable = True
-        self.initial_data['helper'] = self._get_helper_profile().id
+
+        self.initial_data['helper'] = self.helper.id
         self.initial_data['status'] = Offer.StatusTypes.PENDING
         return super().is_valid(*args, **kwargs)
 
     def validate(self, data):
         task = data['task']
-        profile_id = self._get_helper_profile().id
+        profile_id = self.helper.id
 
         self._validate_helper(task, profile_id)
         return super().validate(data)
@@ -42,9 +44,7 @@ class OfferSerializer(serializers.ModelSerializer):
             )
 
     def _validate_helper_has_matching_service(self, task, profile_id):
-        helper = self._get_helper_profile()
-
-        if task.service not in helper.services.all():
+        if task.service not in self.helper.services.all():
             raise serializers.ValidationError(
                 [
                     'You cannot create offer because you do not have a matching service.'  # noqa
