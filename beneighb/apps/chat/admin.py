7255aae1f0ca35903d6acf432fb1d325b4df225b
Myroslav Hryshyn <mhryshyn@gmail.com>
2024-01-04 17:11:39 +0100

    feat(chat): Add admin for Chat and Message

--- beneighb/apps/chat/admin.py
+++ beneighb/apps/chat/admin.py
@@ -1,1 +1,32 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+
+from .models import Chat, Message
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_offer_helper', 'get_task_owner', 'created_at']
+
+    def get_task_owner(self, obj):
+        if obj.assignment:
+            return obj.assignment.offer.task.owner
+
+    def get_offer_helper(self, obj):
+        if obj.assignment:
+            return obj.assignment.offer.task.owner
+
+    get_task_owner.short_description = 'Task owner'
+    get_offer_helper.short_description = 'Offer helper'
+
+
+admin.site.register(
+    Message,
+    list_display=[
+        'id',
+        'author',
+        'chat',
+        'sent_at',
+        'read_at',
+        'text',
+    ],
+)
