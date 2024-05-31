66096b3905e0e8578e4d6a05d9ec1374b84f2af6
Mero <mhryshyn@gmail.com>
2024-05-31 09:43:44 +0000

    chore: Fix start_prod to run migrations

--- start_prod.sh
+++ start_prod.sh
@@ -6,5 +6,5 @@ docker-compose -f docker-compose-images.prod.yml up -d
 
 sleep 3
 
-docker-compose exec web python manage.py migrate --noinput
+docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
 docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
